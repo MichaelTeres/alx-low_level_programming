5fdfb00a7711fad4ca9ca015322eaa560328c274
MichaelTeres <nwadinigwemichael@yahoo.com>
2022-10-17 07:38:20 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char lowerCase;
+for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+putchar(low);
+putchar ('\n');
+return (0);
+}
