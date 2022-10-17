efc50472e83b5661fac9e53f6a8e84933ac38fad
MichaelTeres <nwadinigwemichael@yahoo.com>
2022-10-17 08:29:33 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char low;
+	char up;
+	for(low = 'a'; low =< 'z'; low++)
+		putchar(low);
+	for(up = 'A'; up <= 'Z'; up++)
+		putchar(up);
+	putchar('\n');
+	return(0);
+}
