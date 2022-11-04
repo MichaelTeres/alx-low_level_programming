d54b9415d756e09eb364bfda2f456e4dd74829ba
MichaelTeres <nwadinigwemichael@yahoo.com>
2022-11-04 15:47:00 -0700

    library

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -11,20 +11,4 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	if ((s == NULL) || (accept == NULL))
-	{
-		return (NULL);
-	}
-	while (*s)
-	{
-		if (_strchr(accept, *s))
-		{
-			return (s);
-		}
-		else
-		{
-			s++;
-		}
-	}
-	return (NULL);
 }
