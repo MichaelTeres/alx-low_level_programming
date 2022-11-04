d54e2f25f624ba152ba865704009faa37562d9ca
MichaelTeres <nwadinigwemichael@yahoo.com>
2022-11-04 15:39:19 -0700

    library

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -2,29 +2,12 @@
 #include <stddef.h>
 
 /**
- * _strchr - function that locates a character in a string.
- * *_strpbrk - function that searches a string for any of a set of bytes.
+ *_strpbrk - function that searches a string for any of a set of bytes.
  * @s: An input character
- * @c: An input character
  * @accept: An input character
  * Return: the number of bytes in the initial segment of s,
  * which consist only of bytes from accept
  */
-char *_strchr(char *s, char c)
-{
-	while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
-	return (NULL);
-}
-
-
 
 char *_strpbrk(char *s, char *accept)
 {
