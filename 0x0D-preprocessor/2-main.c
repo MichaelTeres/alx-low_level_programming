017e1fb33f0de88f33a74369c3c1ab15567b1f62
MichaelTeres <nwadinigwemichael@yahoo.com>
2022-11-15 07:02:22 -0800

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that print the name of a file it was compiled from
+ * @agrc: An input variable
+ * @agrv: An input variable
+ *
+ * Return: Always 0
+ */
+
+int _print_file_name(int agrc, char *agrv);
+
+int main()
+{
+	_putchat();
+	return (0);
+}
+
+int _print_file _name(int agrc, char *agrv)
+{
+
